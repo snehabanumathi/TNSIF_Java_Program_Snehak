8aa6217eeb1f3be44eabe9ca8c997db28c85928f
DELL <DELL@DESKTOP-DB6L1EU>
2025-07-03 11:07:44 -0700

    Commit 6

---
+++ TNSIF_Javaprogram/src/com/tnsif/interfacedemo/FunctionalDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.interfacedemo;
+
+public class FunctionalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TNSIF_Javaprogram/src/com/tnsif/interfacedemo/GreetingDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.interfacedemo;
+
+public class GreetingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
